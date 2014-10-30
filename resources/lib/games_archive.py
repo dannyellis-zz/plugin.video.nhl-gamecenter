5e86cc75847078b61476502b197915ca4fb9ceeb
eracknaphobia <eracknaphobia@hotmail.com>
2014-10-30 14:48:55 -0400

    Updated Quality settings
    Include 5k for archived streams

--- resources/lib/games_archive.py
+++ resources/lib/games_archive.py
@@ -153,7 +153,12 @@ def getGameLinks(url):
             
             #Quality settings            
             if QUALITY == 4 or 'bestquality' in url:
-                quality = ''
+                if int(year) >= 2014:
+                    quality = '_5000'
+                elif int(year) >= 2012:
+                    quality = '_4500'
+                else:
+                    quality = '_3000'
             elif '5000K' in url:
                 quality = '_5000' 
             elif QUALITY == 3 or '4500K' in url:
