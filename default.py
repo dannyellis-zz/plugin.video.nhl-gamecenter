6c5c61e95e47bc107ad7bdc52111bf1120a52dfd
eracknaphobia <eracknaphobia@hotmail.com>
2014-11-04 16:00:11 -0500

    Feature Added
    Create a thread that polls the gameScoreboard function for goal updates
    in 60 second intervals. Display's notification to screen with team
    names, score and game clock. Need fine tuned as far as when to launch
    and turn off. For now if the Score Updates setting is set to true when
    the user selects the Live game selection the thread starts and the only
    way to shut it off is to open the settings menu and set Score Updates to
    false.

--- default.py
+++ default.py
@@ -4,9 +4,11 @@
 ############################################################################
 
 from resources.lib.userinterface import *
+from resources.lib.scoring_updates import *
 
 params = get_params()
 
+print params
 try:
     url=urllib.unquote_plus(params["url"])
 except:
@@ -16,24 +18,36 @@ try:
 except:
     mode = None
 
+
 print "Mode: "+str(mode)
 print "URL: "+str(url)
 
-if mode == None or url==None or len(url)<1:
-    #login()
-    CATEGORIES()
+
+if mode == None or url==None or len(url)<1:    
+    #login()   
+    #l.isRunning = False    
+    CATEGORIES()    
+    #Reset setting
+    #ADDON.setSetting(id='score_updates', value='false')
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
+    #Do Not Cache
+    #xbmcplugin.endOfDirectory(int(sys.argv[1]),1,0,0)
 elif mode == 1:
     LIVE(url)
+    #ADDON.setSetting(id='score_updates', value='true')           
+    s = ScoreThread()
+    t = threading.Thread(target = s.Scoring_Updates)
+    t.start()        
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 elif mode == 2:
     LIVEQUALITY(url)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 elif mode == 3:
-    LIVELINKS(url)
+    LIVELINKS(url)    
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 elif mode == 4:
     ARCHIVE(url)
+    #ADDON.setSetting(id='score_updates', value='false')
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 elif mode == 5:
     ARCHIVEMONTH(url)
---
+++ resources/lib/scoring_updates.py
@@ -0,0 +1,64 @@
+import threading
+import xbmcgui
+import xbmc
+from resources.lib.globals import *
+from resources.lib.games_live import *
+from time import sleep
+from datetime import datetime
+
+class ScoreThread(object):
+
+    def Scoring_Updates(self):
+       #while self.isRunning == True:
+       #print "SHOW SCORE SETTINGS == " + ADDON.getSetting(id="score_updates")
+       FIRST_TIME_THRU = 1       
+       OLD_GAME_STATS = []   
+       todays_date = datetime.now().strftime("%Y-%m-%d")       
+
+       while ADDON.getSetting(id="score_updates") == 'true':        
+        print "do stuff here"
+        print todays_date
+        json_source = getScoreBoard(todays_date)
+        dialog = xbmcgui.Dialog()                        
+        NEW_GAME_STATS = []
+        refreshInterval = json_source['refreshInterval']
+        for game in json_source['games']:                    
+            gid = str(game['id'])
+            ateam = game['ata']
+            hteam = game['hta']
+            ascore = '[COLOR=FF00B7EB]'+str(game['ats'])+'[/COLOR]'
+            hscore = '[COLOR=FF00B7EB]'+str(game['hts'])+'[/COLOR]'
+            gameclock = game['bs']                           
+            NEW_GAME_STATS.append([gid,ateam,hteam,ascore,hscore,gameclock])
+
+
+        if FIRST_TIME_THRU == 1:
+            print "FIRST TIME"           
+            FIRST_TIME_THRU = 0
+        else:
+            for new_item in NEW_GAME_STATS:
+                #print new_item
+                for old_item in OLD_GAME_STATS:                    
+                    if new_item[0] == old_item[0]:
+                        #print "We have a match"
+                        #print new_item[0] + " == "+ old_item[0]
+                        #print "is " + new_item[5] + " == " + old_item[5]
+                        if new_item[3] != old_item[3] or new_item[4] != old_item[4] or 1 == 1:
+                            #or new_item[5] != old_item[5]:                            
+                            ateam = new_item[1]
+                            hteam = new_item[2]
+                            ascore = new_item[3]
+                            hscore = new_item[4]
+                            gameclock = new_item[5]
+                            message = ateam + ' ' + ascore + '    ' + hteam + ' ' + hscore + '    ' + gameclock                    
+                            dialog.notification('Score Update', message, ADDON_PATH+'/resources/images/nhl_logo.png', 5000, False)
+                            sleep(5)
+
+        OLD_GAME_STATS = []
+        OLD_GAME_STATS = NEW_GAME_STATS
+        #xbmc.sleep(int(refreshInterval)*1000)        
+        sleep(int(refreshInterval))
+        #print "SHOW SCORE SETTINGS == " + ADDON.getSetting(id="score_updates")
+        #win = xbmcgui.Window(xbmcgui.getCurrentWindowId())
+        #print str(win)
+        #print "IS PLAYING VIDEO = " + str(xbmc.Player.isPlayingVideo())
